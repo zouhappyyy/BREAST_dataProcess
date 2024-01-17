fbdd99fc6f1dff68817f802eb105eb0dcfdc3bea
cs1807cwy <43921644+cs1807cwy@users.noreply.github.com>
2024-01-17 20:34:42 +0800

    添加对导出NIFTI图像的转换筛选控制，去除非常规模态

--- data_convert.py
+++ data_convert.py
@@ -15,26 +15,39 @@ def extract_dicom_info(dicom_file_path: str) -> Optional[Dict[str, Any]]:
 
     # 如果模态不是钼靶，则丢弃
     if dicom_data.Modality != 'MG':
-        print(f'Exclude: Modality except MG but {dicom_data.Modality} -> {dicom_file_path}')
+        print(f'Exclude: Modality expect MG but {dicom_data.Modality} -> {dicom_file_path}')
         return None
 
     try:
         # DICOM文件的基本信息
         extracted_info: Dict[str, Any] = {
+            '登记号': dicom_data.AccessionNumber,
             '病人标识符': dicom_data.PatientID,
             '序列号': dicom_data.SeriesNumber,
             '实例号': dicom_data.InstanceNumber,
+            '字符集': dicom_data.SpecificCharacterSet,
+            '生成日期': dicom_data.ContentDate,
+            '生成时间': dicom_data.ContentTime,
             '成像类型': dicom_data.ImageType,
             '图像偏侧性': dicom_data.ImageLaterality,
             '投照体位': dicom_data.ViewPosition,
-            '采集设备处理代码': dicom_data['AcquisitionDeviceProcessingCode'].value,  # Acquisition Device Processing Code
+            '临床视角': dicom_data[0x0045, 0x101b].value if (0x0045, 0x101b) in dicom_data else 'UND',  # Clinical View
+            '采集设备处理代码': dicom_data['AcquisitionDeviceProcessingCode'].value if 'AcquisitionDeviceProcessingCode' in dicom_data else 'UND',
+            # [AUTOMATIC, MANUAL]  手动/自动曝光
+            '曝光控制模式': dicom_data['ExposureControlMode'].value if 'ExposureControlMode' in dicom_data else 'UND',
+            # [LIN, LOG] X射线强度到像素值的映射方式，常见的有线性和对数两种
+            '像素明度相关性': dicom_data['PixelIntensityRelationship'].value if 'PixelIntensityRelationship' in dicom_data else 'UND',
             '检查部位': dicom_data.BodyPartExamined,
             '模态': dicom_data.Modality,
         }
     # 丢弃故障数据
     except KeyError as e:
-        print(f'Exclude: Key missing -> {dicom_file_path}')
-        # print(dicom_data)
+        print(f'Exclude: Key \'{e}\' missing -> {dicom_file_path}')
+        return None
+
+    if extracted_info['临床视角'] != 'UND' and extracted_info['图像偏侧性'] + extracted_info['投照体位'] != extracted_info['临床视角']:
+        print(
+            f'Exclude: Device view {extracted_info["图像偏侧性"]}+{extracted_info["投照体位"]} not equal to Clinical view {extracted_info["临床视角"]}')
         return None
 
     return extracted_info
@@ -43,11 +56,16 @@ def extract_dicom_info(dicom_file_path: str) -> Optional[Dict[str, Any]]:
 # 合成样本名称
 def merge_sample_name(extracted_info: Dict[str, Any]) -> str:
     acquisition_device_processing_code: str = extracted_info['采集设备处理代码']
-    short_processing_code: str = str.split(acquisition_device_processing_code, '_')[1]
+    short_processing_code: str = 'UND' if acquisition_device_processing_code == 'UND' else str.split(acquisition_device_processing_code, '_')[1]
     sample_name: str = '_'.join([
+        extracted_info['登记号'],
         extracted_info['病人标识符'],
-        extracted_info['投照体位'],
+        extracted_info['曝光控制模式'],
+        extracted_info['像素明度相关性'],
+        extracted_info['生成日期'],
+        extracted_info['生成时间'],
         extracted_info['图像偏侧性'],
+        extracted_info['投照体位'],
         short_processing_code,
     ])
     return sample_name
@@ -57,6 +75,7 @@ def merge_sample_name(extracted_info: Dict[str, Any]) -> str:
 def generate_data_source_indexer(source_root_path: str) -> List[Dict[str, Any]]:
     indexer: List[Dict[str, Any]] = []
     tot_count: int = 0
+    convert_count: int = 0
     with tqdm.tqdm(desc='Indexing') as pbar:
         for root, dirs, files in os.walk(source_root_path):
             # print((root, dirs, files))
@@ -72,16 +91,23 @@ def generate_data_source_indexer(source_root_path: str) -> List[Dict[str, Any]]:
                     continue
                 accession_number: str = osp.split(root)[-1]
                 item: Dict[str, Any] = {
-                    'accession_number': accession_number,
-                    'dcm_file_name': dcmf,
-                    'dcm_rel_path': osp.relpath(dcm_abs_path, source_root_path),
-                    'target_rel_path': osp.join(accession_number, merge_sample_name(extracted_info))
+                    '登记号': accession_number,
+                    'DICOM源文件名': dcmf,
+                    'DICOM相对路径': osp.relpath(dcm_abs_path, source_root_path),
+                    'Nifti相对路径': ''
                 }
+                if extracted_info['成像类型'][1] == 'PRIMARY' and \
+                        extracted_info['成像类型'][3] in {'LOW_ENERGY', 'RECOMBINED'} and \
+                        extracted_info['采集设备处理代码'] in {'GEMS_FFDM_PV', 'GEMS_CESM_1'} and \
+                        extracted_info['像素明度相关性'] == 'LOG':
+                    item['Nifti相对路径'] = osp.join(accession_number, merge_sample_name(extracted_info))
+                    convert_count += 1
+                item.update(extracted_info)
                 indexer.append(item)
                 pbar.update(1)
                 tot_count += 1
 
-    print(f'Total: {tot_count}, Valid: {len(indexer)}, Excluded: {tot_count - len(indexer)}')
+    print(f'Total: {tot_count}, Convert: {convert_count}, Valid: {len(indexer)}, Excluded: {tot_count - len(indexer)}')
     return indexer
 
 
@@ -96,17 +122,20 @@ def indexer_to_dataframe(indexer: List[Dict[str, Any]]) -> pd.DataFrame:
 def dicom_convert_to_nii(indexer: List[Dict[str, Any]], source_root_path: str, target_root_path: str):
     """
     indexer: {
-        'accession_number': UCRxxxxxxxxxxxx作为目录,
-        'dcm_file_name': 源文件名称（带扩展名，不带目录）,
-        'dcm_rel_path': 源文件相对于数据源根的相对路径,
-        'target_rel_path': 输出到指定根相对路径
+        '登记号': UCRxxxxxxxxxxxx作为目录,
+        'DICOM源文件名': 源文件名称（带扩展名，不带目录）,
+        'DICOM相对路径': 源文件相对于数据源根的相对路径,
+        'Nifti相对路径': 输出到指定根相对路径
     }
     """
     with tqdm.tqdm(total=len(indexer), desc='Converting') as pbar:
         for idx in indexer:
-            abs_target_dir_path: str = osp.abspath(osp.join(target_root_path, idx['accession_number']))
-            abs_source_file_path: str = osp.abspath(osp.join(source_root_path, idx['dcm_rel_path']))
-            abs_target_file_path: str = osp.abspath(osp.join(target_root_path, idx['target_rel_path'] + '.nii.gz'))
+            if idx['Nifti相对路径'] == '':
+                pbar.update(1)
+                continue
+            abs_source_file_path: str = osp.abspath(osp.join(source_root_path, idx['DICOM相对路径']))
+            abs_target_file_path: str = osp.abspath(osp.join(target_root_path, idx['Nifti相对路径'] + '.nii.gz'))
+            abs_target_dir_path: str = osp.dirname(abs_target_file_path)
 
             # 建立目录
             os.makedirs(abs_target_dir_path, exist_ok=True)
